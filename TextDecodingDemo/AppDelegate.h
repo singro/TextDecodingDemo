c1f1d93fb0e180125f3b2d41d018dc3adb6658d1
Singro <xingrong.cxr@alibaba-inc.com>
2017-05-10 11:01:01 +0800

    Initial Commit

---
+++ TextDecodingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextDecodingDemo
+//
+//  Created by Singro on 10/05/2017.
+//  Copyright © 2017 Singro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextDecodingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextDecodingDemo
+//
+//  Created by Singro on 10/05/2017.
+//  Copyright © 2017 Singro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
