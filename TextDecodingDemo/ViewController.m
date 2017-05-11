5c4706ba96c7c61295a4bcc0f0f36ded709d1d4c
Singro <xingrong.cxr@alibaba-inc.com>
2017-05-11 14:16:32 +0800

    Init

--- TextDecodingDemo/ViewController.m
+++ TextDecodingDemo/ViewController.m
@@ -16,7 +16,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSString *boundlePath = [[NSBundle mainBundle] pathForResource:@"Untitled" ofType:@"txt"];
+    
+    NSData *data = [NSData dataWithContentsOfFile:boundlePath];
+    NSStringEncoding encoding = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingGB_18030_2000);
+    
+    
+    NSString *content = [[NSString alloc] initWithData:data encoding:encoding];
+    NSLog(@"content: %@", content);
+    
+    
+    // writeToFile test
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *path = paths.firstObject;
+    path = [path stringByAppendingString:@"/writeToFile.txt"];
+    
+    NSLog(@"path:\n%@", path);
+    NSError *error2 = nil;
+    
+    [content writeToURL:[NSURL fileURLWithPath:path] atomically:YES encoding:encoding error:&error2];
+    if (error2)
+    {
+        NSLog(@"writeToFile failure");
+    }
+    
+    
+    NSInteger contentSize = [content lengthOfBytesUsingEncoding:encoding];
+    
+    NSLog(@"size: %zd", contentSize);
+    
+    if ([content canBeConvertedToEncoding:encoding])
+    {
+        NSLog(@"can...");
+    }
+    
+    
+    @try {
+        
+        NSData *data2 = [content dataUsingEncoding:encoding allowLossyConversion:YES];
+        NSLog(@"data2: %@", data2);
+        
+    } @catch (NSException *exception) {
+        
+        NSLog(@"exception: %@", exception);
+        
+    } @finally {
+        
+    }
 }
 
 
